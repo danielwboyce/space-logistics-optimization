3ab25fce21978dd47826320ab226c7ded74c380c
Daniel Boyce <danielwboyce@gmail.com>
2026-04-11 19:16:39 -0400

    try and maximize the amount of LOX in LEO at the end

--- src/opt_model_builder/objective.py
+++ src/opt_model_builder/objective.py
@@ -4,6 +4,7 @@ from pyomo.kernel import (
     objective,
     constraint,
     minimize,
+    maximize,
     block,
 )
 from pyomo.core.expr.numeric_expr import SumExpression
@@ -25,8 +26,8 @@ class Objective:
             m: pyomo.kernel model
         """
         if self.builder.is_stochastic:
-            m.imleo_def = constraint(
-                m.imleo
+            m.fmleo_def = constraint(
+                m.fmleo
                 == (
                     self._get_obj_term(m, 0, self.builder.first_mis_time_steps)
                     + sum(
@@ -39,9 +40,9 @@ class Objective:
                 )
             )
         else:
-            m.imleo_def = constraint(m.imleo == (self._get_obj_term(m, 0, m.time_idx)))
+            m.fmleo_def = constraint(m.fmleo == (self._get_obj_term(m, 0, m.time_idx)))
         if self.builder.mode != "ALCsubproblem":
-            m.obj = objective(m.imleo, sense=minimize)
+            m.obj = objective(m.fmleo, sense=maximize)
         else:
             # WARNING: For some reason, the augmented objective function
             # CANNOT be defined as a constraint due to its quadratic nature.
@@ -49,7 +50,7 @@ class Objective:
             # by Gurobi 10, even though it is not.
             m.obj = objective(
                 expr=(
-                    m.imleo
+                    m.fmleo
                     + sum(
                         m.lag_mult[sc_des, sc_var] * m.rel_consis_vio[sc_des, sc_var]
                         for sc_des in m.sc_des_idx
@@ -65,13 +66,13 @@ class Objective:
                         for sc_var in m.sc_var_idx
                     )
                 ),
-                sense=minimize,
+                sense=maximize,
             )
         return m
 
     def _get_obj_term(self, m: block, scnr: int, time_list: list[int]) -> SumExpression:
-        """Returns sum of commodities and sc mass launched from Earth to LEO
-        for a specific scenario over given time interval.
+        """Returns sum of LOX that is present in the LEO node at the final
+        time step.
 
         Args:
             m: pyomo.kernel model
@@ -80,59 +81,29 @@ class Objective:
         Returns:
             SumExpression: sum of commodities and sc mass launched to LEO
         """
+        cnt_com = self.builder.cnt_com_dict["oxygen"]
+        i = self.builder.node_dict["LEO"]
+        t = time_list[-1]
+        t_id = len(time_list) - 1
+        io = self.builder.flow_dict["in"]
         term = (
-            sum(
-                self.builder.int_com_costs[int_com]
-                * sum(
-                    m.int_com[
-                        sc_des,
-                        sc_cp,
-                        self.builder.node_dict["Earth"],
-                        self.builder.node_dict["LEO"],
-                        int_com,
-                        self.builder.flow_dict["out"],
-                        t,
-                        scnr,
-                    ]
-                    for sc_des in m.sc_des_idx
-                    for sc_cp in m.sc_copy_idx
-                    for t in time_list
-                )
-                for int_com in m.int_com_idx
-            )
-            + sum(
-                self.builder.cnt_com_costs[cnt_com]
-                * sum(
-                    m.cnt_com[
-                        sc_des,
-                        sc_cp,
-                        self.builder.node_dict["Earth"],
-                        self.builder.node_dict["LEO"],
-                        cnt_com,
-                        self.builder.flow_dict["out"],
-                        t,
-                        scnr,
-                    ]
-                    for sc_des in m.sc_des_idx
-                    for sc_cp in m.sc_copy_idx
-                    for t in time_list
-                )
-                for cnt_com in m.cnt_com_idx
-            )
-            + sum(
-                m.sc_fly_var[
+            self.builder.cnt_com_costs[cnt_com]
+            * sum(
+                m.cnt_com[
                     sc_des,
                     sc_cp,
-                    self.builder.sc_var_dict["dry mass"],
-                    self.builder.node_dict["Earth"],
-                    self.builder.node_dict["LEO"],
-                    self.builder.flow_dict["out"],
-                    t,
+                    j,
+                    i,
+                    cnt_com,
+                    io,
+                    t - self.builder.delta_t[i][j][t_id],
                     scnr,
                 ]
                 for sc_des in m.sc_des_idx
                 for sc_cp in m.sc_copy_idx
-                for t in time_list
+                for j in m.arr_node_idx
+                if self.builder.is_feasible_arc(i, j)
+                if t - self.builder.delta_t[i][j][t_id] in m.time_idx
             )
         )
         return term
